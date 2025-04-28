6c435a17b20b70dd4e35d05c4ec794ef43503fe6
Amanda Wang <amanda.m.wang21@gmail.com>
2025-04-28 13:41:18 -0700

    sometimes fitdist fails for fitting zip and zinb distributions, so if a try-error is returned, we now retry the function with modified sigma parameters in the starting values

--- R/whichdist.R
+++ R/whichdist.R
@@ -22,6 +22,39 @@
 
 
 whichdist <- function(df, countvar, diagnostics = FALSE) {
+  # This is a helper function that first attempts fitdist() for ZIP and ZINB distributions.
+  # checks if the try() threw an error, and if so, retries with a modified upper() parameter list
+
+  safe_fitdist <- function(x, distname, start, lower, upper, retry_upper = NULL) {
+    fit <- try(
+      fitdistrplus::fitdist(
+        x, distname, start = start,
+        discrete = TRUE, lower = lower, upper = upper
+      ),
+      silent = TRUE
+    )
+
+    if (inherits(fit, "try-error") && !is.null(retry_upper)) {
+      # Retry with modified upper
+      fit <- try(
+        fitdistrplus::fitdist(
+          x, distname, start = start,
+          discrete = TRUE, lower = lower, upper = retry_upper
+        ),
+        silent = TRUE
+      )
+    }
+
+    if (inherits(fit, "try-error")) {
+      warning(
+        glue::glue("Both attempts to fit {distname} distribution failed.")
+      )
+      return(NULL)
+    }
+
+    return(fit)
+  }
+
   suppressWarnings({
     options(stringsAsFactors = F)
 
@@ -78,132 +111,143 @@ whichdist <- function(df, countvar, diagnostics = FALSE) {
       tmp.fit.nbinom.wo <- try(fitdistrplus::fitdist(data = df[,col_ind], 'nbinom'), silent = TRUE)
 
       if (min(df[,col_ind])==0) {
-        # fit zero inflated poisson with fitdistrplus
-        tmp.fit.zifpois.wo <- try(fitdistrplus::fitdist(df[,col_ind], "ZIP", start = list(mu = estmu, sigma=p0),
-                                                        discrete = TRUE, lower = c(0, 0), upper = c(Inf, 1)), silent = TRUE)
-        # fit zero inflated negative binomial with fitdistrplus
-        tmp.fit.zifnbinom.wo <- try(fitdistrplus::fitdist(df[,col_ind], "ZINBI", start = list(mu = estmu, sigma=disp, nu=p0),
-                                                          discrete = TRUE, lower = c(0, 0, 0), upper = c(Inf,Inf, 1)), silent = TRUE)
+        tmp.fit.zifpois.wo <- safe_fitdist(x = df[, col_ind],
+                                           distname = "ZIP",
+                                           start = list(mu = estmu, sigma = p0),
+                                           lower = c(0, 0),
+                                           upper = c(Inf, 1),
+                                           retry_upper = c(Inf, p0 * 10)
+        )
+
+        tmp.fit.zifnbinom.wo <- safe_fitdist(x = df[, col_ind],
+                                             distname = "ZINBI",
+                                             start = list(mu = estmu, sigma = disp, nu = p0),
+                                             lower = c(0, 0, 0),
+                                             upper = c(Inf, Inf, 1),
+                                             retry_upper = c(Inf, disp * 10, 1)
+        )
       }
 
-
-      # lm with fitdistr
-      lm.gof.fitdistr <- summary(tmp.fit.lm.wo)
-      lmfitdistr.loglik <-  try(lm.gof.fitdistr$loglik)
-      lmfitdistr.aic <-  try(lm.gof.fitdistr$aic)
-      lmfitdistr_est <- try(tmp.fit.lm.wo$estimate)
-      gof.res[, grep("^normal_fitdistr", colnames(gof.res), perl=TRUE)] <-  rbind(lmfitdistr.loglik, lmfitdistr.aic)
-      estimate.res[, grep("^normal", colnames(estimate.res), perl=TRUE)] <- rbind(lmfitdistr_est[1],lmfitdistr_est[2])
-
-      # poisson with fitdistr
-      pois.gof.fitdistr <- summary(tmp.fit.pois.wo)
-      poisfitdistr.loglik <-  try(pois.gof.fitdistr$loglik)
-      poisfitdistr.aic <- try(pois.gof.fitdistr$aic)
-      poisfitdistr_est <- try(tmp.fit.pois.wo$estimate)
-      poisfitdistr_sd <- try(tmp.fit.pois.wo$sd)
-      gof.res[, grep("^pois_fitdistr", colnames(gof.res), perl=TRUE)] <-  rbind(poisfitdistr.loglik, poisfitdistr.aic)
-      estimate.res[, grep("^pois", colnames(estimate.res), perl=TRUE)] <- rbind(poisfitdistr_est, poisfitdistr_sd)
-
-      # neg binom with fitdistr
-      nbinom.gof.fitdistr <- try(summary(tmp.fit.nbinom.wo))
-      nbinomfitdistr.loglik <-  try(nbinom.gof.fitdistr$loglik)
-      nbinomfitdistr.aic <- try(nbinom.gof.fitdistr$aic)
-      nbinomfitdistr_est <- try(tmp.fit.nbinom.wo$estimate)
-      gof.res[, grep("^nbinom_fitdistr", colnames(gof.res), perl=TRUE)] <-  rbind(nbinomfitdistr.loglik, nbinomfitdistr.aic)
-      estimate.res[, grep("^nbinom", colnames(estimate.res), perl=TRUE)] <- nbinomfitdistr_est
-
-
-      if (min(df[, col_ind])==0) {
-        # zero inflated poisson with fitdistr
-        zifpois.gof.fitdistr <- summary(tmp.fit.zifpois.wo)
-        zifpoisfitdistr.loglik <-  zifpois.gof.fitdistr$loglik
-        zifpoisfitdistr.aic <-  zifpois.gof.fitdistr$aic
-        zifpoisfitdistr_est <- tmp.fit.zifpois.wo$estimate
-        gof.res[, grep("^zifpois_fitdistr", colnames(gof.res), perl = TRUE)] <- rbind(zifpoisfitdistr.loglik, zifpoisfitdistr.aic)
-        estimate.res[, grep("^zifpois", colnames(estimate.res), perl = TRUE)] <- zifpoisfitdistr_est
-
-        # zero inflated neg binom with fitdistr
-        zifnbinom.gof.fitdistr <- summary(tmp.fit.zifnbinom.wo)
-        zifnbfitdistr.loglik <-  zifnbinom.gof.fitdistr$loglik
-        zifnbfitdistr.aic <-  zifnbinom.gof.fitdistr$aic
-        zifnbfitdistr_est <- tmp.fit.zifnbinom.wo$estimate
-        gof.res[, grep("^zifnbinom_fitdistr", colnames(gof.res), perl = TRUE)] <- rbind(zifnbfitdistr.loglik, zifnbfitdistr.aic)
-        estimate.res[, grep("^zifnb", colnames(estimate.res), perl = TRUE)] <- zifnbfitdistr_est
-      }
-      # LR Test for P > Norm ?
-      tmp.lrt.pnorm = try(pchisq(as.numeric(2 * (logLik(tmp.fit.pois.wo ) - logLik(tmp.fit.lm.wo))),
-                                 df = length(tmp.fit.pois.wo$estimate)-length(tmp.fit.lm.wo$estimate),
-                                 lower.tail = FALSE))
-      lrt_res[, grep("LRT_PvsNorm", colnames(lrt_res), perl=TRUE)] <- tmp.lrt.pnorm
-
-      # LR Test for NB > Norm ?
-      tmp.lrt.nbnorm = try(pchisq(as.numeric(2 * (logLik(tmp.fit.nbinom.wo ) - logLik(tmp.fit.lm.wo))),
-                                  df = length(tmp.fit.nbinom.wo$estimate)-length(tmp.fit.lm.wo$estimate),
-                                  lower.tail = FALSE))
-      lrt_res[, grep("LRT_NBvsNorm", colnames(lrt_res), perl=TRUE)] <- tmp.lrt.nbnorm
-
-      # LR Test for NB > P ?
-      tmp.lrt.nbp = try(pchisq(as.numeric(2 * (logLik(tmp.fit.nbinom.wo) - logLik(tmp.fit.pois.wo))),
-                               df = length(tmp.fit.nbinom.wo$estimate)-length(tmp.fit.pois.wo$estimate),
-                               lower.tail = FALSE))
-      lrt_res[, grep("LRT_NBvsP", colnames(lrt_res), perl=TRUE)] <- tmp.lrt.nbp
-
-      if (min(df[, col_ind])==0) {
-        # LR Test for ZINB > NB ?
-        tmp.lrt.nbzinb = try(pchisq(as.numeric(2 * (logLik(tmp.fit.zifnbinom.wo) - logLik(tmp.fit.nbinom.wo))),
-                                    df = length(tmp.fit.zifnbinom.wo$estimate)-length(tmp.fit.nbinom.wo$estimate), lower.tail = FALSE))
-        lrt_res[, grep("LRT_ZINBvsNB", colnames(lrt_res), perl=TRUE)] <- tmp.lrt.nbzinb
-
-        # LR Test for ZIP > P ?
-        tmp.lrt.zipp = try(pchisq(as.numeric(2 * (logLik(tmp.fit.zifpois.wo) - logLik(tmp.fit.pois.wo))),
-                                  df = length(tmp.fit.zifpois.wo$estimate)-length(tmp.fit.pois.wo$estimate),
-                                  lower.tail = FALSE))
-        lrt_res[, grep("LRT_ZIPvsP", colnames(lrt_res), perl=TRUE)] <- tmp.lrt.zipp
-
-        # LR Test for ZINB > ZIP ?
-        tmp.lrt.zinbzip = try(pchisq(as.numeric(2 * (logLik(tmp.fit.zifnbinom.wo) - logLik(tmp.fit.zifpois.wo))),
-                                     df = length(tmp.fit.zifnbinom.wo$estimate)-length(tmp.fit.zifpois.wo$estimate),
-                                     lower.tail = FALSE))
-        lrt_res[, grep("LRT_ZINBvsZIP", colnames(lrt_res), perl=TRUE)] <- tmp.lrt.zinbzip
-      }
-
-      lowest_aic_model <- colnames(gof.res[gof.res$metric == "aic",c(2:ncol(gof.res))])[apply(gof.res[gof.res$metric == "aic",c(2:ncol(gof.res))], 1, which.min)]
-      if (grepl(pattern = "normal", x = lowest_aic_model, ignore.case = T)) {
-        suggested_model_message <- "normal distribution: try a t-test, linear regression, or ANOVA"
-        suggested_model <- "lm"
-      }
-
-      if (grepl(pattern = "^pois", x = lowest_aic_model, ignore.case = T)) {
-        suggested_model_message <- "Poisson distribution: try glm() with family = 'poisson'"
-        suggested_model <- "poisson"
-      }
-
-      if (grepl(pattern = "^nbinom", x = lowest_aic_model, ignore.case = T)) {
-        suggested_model_message <- "negative binomial distribution: try glm.nb() from MASS package"
-        suggested_model <- "negbin"
-      }
-
-      if (grepl(pattern = "^zifpois", x = lowest_aic_model, ignore.case = T)) {
-        suggested_model_message <- "zero-inflated Poisson distribution: try zeroinfl() from pscl package with dist = 'poisson'"
-        suggested_model <- "zip"
-      }
-
-      if (grepl(pattern = "^zifnbinom", x = lowest_aic_model, ignore.case = T)) {
-        suggested_model_message <- "zero-inflated negative binomial distribution: try zeroinfl() from pscl package with dist = 'negbin'"
-        suggested_model <- "zinb"
-      }
-
-      # list result object:
-      if (diagnostics == TRUE) {
-        return(list(goodness_of_fit_metrics=gof.res, likelihood_ratio_test_results = lrt_res, estimates=estimate.res,
-                    recommendation = paste0("results suggest that the variable ", countvar, " best fits a ", suggested_model_message),
-                    model = suggested_model))
-      }
-      if (diagnostics == FALSE) {
-        return(list(recommendation = paste0("results suggest that the variable ", countvar, " best fits a ", suggested_model_message),
-                    model = suggested_model))
-      }
     }
-  })
+
+
+    # lm with fitdistr
+    lm.gof.fitdistr <- summary(tmp.fit.lm.wo)
+    lmfitdistr.loglik <-  try(lm.gof.fitdistr$loglik)
+    lmfitdistr.aic <-  try(lm.gof.fitdistr$aic)
+    lmfitdistr_est <- try(tmp.fit.lm.wo$estimate)
+    gof.res[, grep("^normal_fitdistr", colnames(gof.res), perl=TRUE)] <-  rbind(lmfitdistr.loglik, lmfitdistr.aic)
+    estimate.res[, grep("^normal", colnames(estimate.res), perl=TRUE)] <- rbind(lmfitdistr_est[1],lmfitdistr_est[2])
+
+    # poisson with fitdistr
+    pois.gof.fitdistr <- summary(tmp.fit.pois.wo)
+    poisfitdistr.loglik <-  try(pois.gof.fitdistr$loglik)
+    poisfitdistr.aic <- try(pois.gof.fitdistr$aic)
+    poisfitdistr_est <- try(tmp.fit.pois.wo$estimate)
+    poisfitdistr_sd <- try(tmp.fit.pois.wo$sd)
+    gof.res[, grep("^pois_fitdistr", colnames(gof.res), perl=TRUE)] <-  rbind(poisfitdistr.loglik, poisfitdistr.aic)
+    estimate.res[, grep("^pois", colnames(estimate.res), perl=TRUE)] <- rbind(poisfitdistr_est, poisfitdistr_sd)
+
+    # neg binom with fitdistr
+    nbinom.gof.fitdistr <- try(summary(tmp.fit.nbinom.wo))
+    nbinomfitdistr.loglik <-  try(nbinom.gof.fitdistr$loglik)
+    nbinomfitdistr.aic <- try(nbinom.gof.fitdistr$aic)
+    nbinomfitdistr_est <- try(tmp.fit.nbinom.wo$estimate)
+    gof.res[, grep("^nbinom_fitdistr", colnames(gof.res), perl=TRUE)] <-  rbind(nbinomfitdistr.loglik, nbinomfitdistr.aic)
+    estimate.res[, grep("^nbinom", colnames(estimate.res), perl=TRUE)] <- nbinomfitdistr_est
+
+
+    if (min(df[, col_ind])==0) {
+      # zero inflated poisson with fitdistr
+      zifpois.gof.fitdistr <- summary(tmp.fit.zifpois.wo)
+      zifpoisfitdistr.loglik <-  zifpois.gof.fitdistr$loglik
+      zifpoisfitdistr.aic <-  zifpois.gof.fitdistr$aic
+      zifpoisfitdistr_est <- tmp.fit.zifpois.wo$estimate
+      gof.res[, grep("^zifpois_fitdistr", colnames(gof.res), perl = TRUE)] <- rbind(zifpoisfitdistr.loglik, zifpoisfitdistr.aic)
+      estimate.res[, grep("^zifpois", colnames(estimate.res), perl = TRUE)] <- zifpoisfitdistr_est
+
+      # zero inflated neg binom with fitdistr
+      zifnbinom.gof.fitdistr <- summary(tmp.fit.zifnbinom.wo)
+      zifnbfitdistr.loglik <-  zifnbinom.gof.fitdistr$loglik
+      zifnbfitdistr.aic <-  zifnbinom.gof.fitdistr$aic
+      zifnbfitdistr_est <- tmp.fit.zifnbinom.wo$estimate
+      gof.res[, grep("^zifnbinom_fitdistr", colnames(gof.res), perl = TRUE)] <- rbind(zifnbfitdistr.loglik, zifnbfitdistr.aic)
+      estimate.res[, grep("^zifnb", colnames(estimate.res), perl = TRUE)] <- zifnbfitdistr_est
+    }
+    # LR Test for P > Norm ?
+    tmp.lrt.pnorm = try(pchisq(as.numeric(2 * (logLik(tmp.fit.pois.wo ) - logLik(tmp.fit.lm.wo))),
+                               df = length(tmp.fit.pois.wo$estimate)-length(tmp.fit.lm.wo$estimate),
+                               lower.tail = FALSE))
+    lrt_res[, grep("LRT_PvsNorm", colnames(lrt_res), perl=TRUE)] <- tmp.lrt.pnorm
+
+    # LR Test for NB > Norm ?
+    tmp.lrt.nbnorm = try(pchisq(as.numeric(2 * (logLik(tmp.fit.nbinom.wo ) - logLik(tmp.fit.lm.wo))),
+                                df = length(tmp.fit.nbinom.wo$estimate)-length(tmp.fit.lm.wo$estimate),
+                                lower.tail = FALSE))
+    lrt_res[, grep("LRT_NBvsNorm", colnames(lrt_res), perl=TRUE)] <- tmp.lrt.nbnorm
+
+    # LR Test for NB > P ?
+    tmp.lrt.nbp = try(pchisq(as.numeric(2 * (logLik(tmp.fit.nbinom.wo) - logLik(tmp.fit.pois.wo))),
+                             df = length(tmp.fit.nbinom.wo$estimate)-length(tmp.fit.pois.wo$estimate),
+                             lower.tail = FALSE))
+    lrt_res[, grep("LRT_NBvsP", colnames(lrt_res), perl=TRUE)] <- tmp.lrt.nbp
+
+    if (min(df[, col_ind])==0) {
+      # LR Test for ZINB > NB ?
+      tmp.lrt.nbzinb = try(pchisq(as.numeric(2 * (logLik(tmp.fit.zifnbinom.wo) - logLik(tmp.fit.nbinom.wo))),
+                                  df = length(tmp.fit.zifnbinom.wo$estimate)-length(tmp.fit.nbinom.wo$estimate), lower.tail = FALSE))
+      lrt_res[, grep("LRT_ZINBvsNB", colnames(lrt_res), perl=TRUE)] <- tmp.lrt.nbzinb
+
+      # LR Test for ZIP > P ?
+      tmp.lrt.zipp = try(pchisq(as.numeric(2 * (logLik(tmp.fit.zifpois.wo) - logLik(tmp.fit.pois.wo))),
+                                df = length(tmp.fit.zifpois.wo$estimate)-length(tmp.fit.pois.wo$estimate),
+                                lower.tail = FALSE))
+      lrt_res[, grep("LRT_ZIPvsP", colnames(lrt_res), perl=TRUE)] <- tmp.lrt.zipp
+
+      # LR Test for ZINB > ZIP ?
+      tmp.lrt.zinbzip = try(pchisq(as.numeric(2 * (logLik(tmp.fit.zifnbinom.wo) - logLik(tmp.fit.zifpois.wo))),
+                                   df = length(tmp.fit.zifnbinom.wo$estimate)-length(tmp.fit.zifpois.wo$estimate),
+                                   lower.tail = FALSE))
+      lrt_res[, grep("LRT_ZINBvsZIP", colnames(lrt_res), perl=TRUE)] <- tmp.lrt.zinbzip
+    }
+
+    lowest_aic_model <- colnames(gof.res[gof.res$metric == "aic",c(2:ncol(gof.res))])[apply(gof.res[gof.res$metric == "aic",c(2:ncol(gof.res))], 1, which.min)]
+    if (grepl(pattern = "normal", x = lowest_aic_model, ignore.case = T)) {
+      suggested_model_message <- "normal distribution: try a t-test, linear regression, or ANOVA"
+      suggested_model <- "lm"
+    }
+
+    if (grepl(pattern = "^pois", x = lowest_aic_model, ignore.case = T)) {
+      suggested_model_message <- "Poisson distribution: try glm() with family = 'poisson'"
+      suggested_model <- "poisson"
+    }
+
+    if (grepl(pattern = "^nbinom", x = lowest_aic_model, ignore.case = T)) {
+      suggested_model_message <- "negative binomial distribution: try glm.nb() from MASS package"
+      suggested_model <- "negbin"
+    }
+
+    if (grepl(pattern = "^zifpois", x = lowest_aic_model, ignore.case = T)) {
+      suggested_model_message <- "zero-inflated Poisson distribution: try zeroinfl() from pscl package with dist = 'poisson'"
+      suggested_model <- "zip"
+    }
+
+    if (grepl(pattern = "^zifnbinom", x = lowest_aic_model, ignore.case = T)) {
+      suggested_model_message <- "zero-inflated negative binomial distribution: try zeroinfl() from pscl package with dist = 'negbin'"
+      suggested_model <- "zinb"
+    }
+
+    # list result object:
+    if (diagnostics == TRUE) {
+      return(list(goodness_of_fit_metrics=gof.res, likelihood_ratio_test_results = lrt_res, estimates=estimate.res,
+                  recommendation = paste0("results suggest that the variable ", countvar, " best fits a ", suggested_model_message),
+                  model = suggested_model))
+    }
+    if (diagnostics == FALSE) {
+      return(list(recommendation = paste0("results suggest that the variable ", countvar, " best fits a ", suggested_model_message),
+                  model = suggested_model))
+    }
+  }
+  )
 }
 
